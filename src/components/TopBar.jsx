a08a057c43aac20d87cdd74703e1b2bcbb1514e0
AhmedIbrahim6012 <aa2559778@gmail.com>
2025-05-21 23:37:28 +0300

    divied to sidebar and topbar and added some icons

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import { styled, alpha } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, InputBase, Stack, useTheme } from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function TopBar({ open, handleDrawerOpen }) {
+ const theme =useTheme()
+
+  return (
+    <AppBar position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={[
+            {
+              marginRight: 5,
+            },
+            open && { display: "none" },
+          ]}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+
+        <Box flexGrow={1} />
+
+        <Stack direction={"row"}>
+          {theme.palette.mode ==="dark" ? (
+            <IconButton color="inherit">
+              <LightModeIcon />
+            </IconButton>
+          ) : (
+            <IconButton color="inherit">
+              <DarkModeIcon />
+            </IconButton>
+          )}
+
+          <IconButton color="inherit">
+            <NotificationsIcon />
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- src/homePageScreen/homePage.jsx
+++ src/homePageScreen/homePage.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import Sidebar from "../components/Sidebar"
+import TopBar from "../components/TopBar";
+
+
+
+
 const HomePage = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+ 
   return (
     <div>
-      <Sidebar/>
+      
+     <TopBar open ={open} handleDrawerOpen={handleDrawerOpen}/>
+
+      <Sidebar open ={open} handleDrawerClose={handleDrawerClose} /> 
+
     </div>
   );
 };
